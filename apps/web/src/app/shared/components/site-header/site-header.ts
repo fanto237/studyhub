2b8655fdc81656709590371a98cdb4056745294d
Lucien <me@fanto.dev>
2026-06-07 11:28:32 +0200

    feat(i18n): add translation support for site header navigation elements

--- apps/web/src/app/shared/components/site-header/site-header.ts
+++ apps/web/src/app/shared/components/site-header/site-header.ts
@@ -2,11 +2,13 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { Icon } from '../icon/icon';
+import { LanguageSelector } from '../language-selector/language-selector';
 import { ThemeToggle } from '../theme-toggle/theme-toggle';
+import { TranslatePipe } from '../../pipes/translate.pipe';
 
 @Component({
   selector: 'app-site-header',
-  imports: [Icon, RouterLink, ThemeToggle],
+  imports: [Icon, LanguageSelector, RouterLink, ThemeToggle, TranslatePipe],
   templateUrl: './site-header.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
